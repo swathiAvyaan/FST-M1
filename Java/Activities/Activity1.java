c9505649c47e628ad155f535d5ba625b7997d18a
swathiAvyaan <129042501+swathiAvyaan@users.noreply.github.com>
2023-04-21 19:38:11 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,13 @@
+package activities;
+
+public class Activity1 {
+    public static void main(String[] args) {
+        Car toyota = new Car();
+        toyota.make = 2014;
+        toyota.color = "Black";
+        toyota.transmission = "Manual";
+        toyota.displayCharacterstics();
+        toyota.accelerate();
+        toyota.brake();
+    }
+}
