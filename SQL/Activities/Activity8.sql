d53f86a66f5f3dc03da7095fee0b1fb41e9ccd6c
swathiAvyaan <129042501+swathiAvyaan@users.noreply.github.com>
2023-04-28 20:58:02 +0530

    Add files via upload

---
+++ SQL/Activities/Activity8.sql
@@ -0,0 +1,92 @@
+REM   Script: Activity8
+REM   activity8
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
+SELECT salesman_id, salesman_city FROM salesman;
+
+SELECT * FROM salesman WHERE salesman_city='Paris';
+
+SELECT salesman_id, commission FROM salesman WHERE salesman_name='Paul Adam';
+
+ALTER TABLE salesman ADD grade int;
+
+UPDATE salesman SET grade=100;
+
+SELECT * FROM salesman;
+
+UPDATE salesman SET grade=200 WHERE salesman_city='Rome';
+
+UPDATE salesman SET grade=300 WHERE salesman_name='James Hoog';
+
+UPDATE salesman SET salesman_name='Pierre' WHERE salesman_name='McLyon';
+
+SELECT * FROM salesman;
+
+select distinct salesman_id from orders;
+
+select order_no, order_date from orders order by order_date;
+
+select order_no, purchase_amount from orders order by purchase_amount DESC;
+
+select * from orders where purchase_amount < 500;
+
+select * from orders where purchase_amount between 1000 and 2000;
+
+select SUM(purchase_amount) AS "Total sum" from orders;
+
+select AVG(purchase_amount) AS "Average" from orders;
+
+select MAX(purchase_amount) AS "Maximum" from orders;
+
+select MIN(purchase_amount) AS "Minumum" from orders;
+
+select COUNT(distinct salesman_id) AS "Total count" from orders;
+
+select SUM(purchase_amount) AS "Total sum" from orders;
+
+select AVG(purchase_amount) AS "Average" from orders;
+
+select MAX(purchase_amount) AS "Maximum" from orders;
+
+select MIN(purchase_amount) AS "Minumum" from orders;
+
+select COUNT(distinct salesman_id) AS "Total count" from orders;
+
+select SUM(purchase_amount) AS "Total sum" from orders;
+
+select AVG(purchase_amount) AS "Average" from orders;
+
+select MAX(purchase_amount) AS "Maximum" from orders;
+
+select MIN(purchase_amount) AS "Minumum" from orders;
+
+select COUNT(distinct salesman_id) AS "Total count" from orders;
+
+select * from orders;
+
+SELECT customer_id, MAX(purchase_amount) AS "Max Amount" FROM orders GROUP BY customer_id;
+
+SELECT salesman_id, order_date, MAX(purchase_amount) AS "Max Amount" FROM orders  
+WHERE order_date=To_DATE('2012/08/17', 'YYYY/MM/DD') GROUP BY salesman_id, order_date;
+
+SELECT customer_id, order_date, MAX(purchase_amount) AS "Max Amount" FROM orders 
+GROUP BY customer_id, order_date 
+HAVING MAX(purchase_amount) IN(2030, 3450, 5760, 6000);
+
